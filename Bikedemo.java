e02bec52b41dc28d20d07a3f4b585624450e6282
Manoj221995 <147677182+Manoj221995@users.noreply.github.com>
2025-03-15 12:23:02 +0530

    Add files via upload

---
+++ Bikedemo.java
@@ -0,0 +1,23 @@
+package Inheritance;
+
+public class Bikedemo {
+
+	public void startbutton()
+	{
+		System.out.println("startbutton manually using kicker ");
+	}
+	
+	public void stopbutton()
+	{
+		System.out.println("stopbutton using is braker");
+	}
+	
+	public void fueltank()
+	{
+		System.out.println("fuel tank maximum capacity 12 liters");
+	}	
+	
+}
+		
+
+
